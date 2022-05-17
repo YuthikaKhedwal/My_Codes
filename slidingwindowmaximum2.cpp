0b47f610df76411da9ac61fd2cbeaa11233097e6
YuthikaKhedwal <87803198+YuthikaKhedwal@users.noreply.github.com>
2022-05-17 15:44:04 +0530

    Add files via upload

---
+++ slidingwindowmaximum2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(auto &i:a)
+    cin>>i;
+    multiset<int,greater<int>>s;
+    vector<int>ans(n-k+1);
+    
+}
