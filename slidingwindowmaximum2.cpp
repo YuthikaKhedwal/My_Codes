8b55ce9c80cbe46649d1106cd087f37da8aec4fe
YuthikaKhedwal <yuthikakhedwal2001@gmail.com>
2022-05-17 15:53:29 +0530

    updated

--- slidingwindowmaximum2.cpp
+++ slidingwindowmaximum2.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main()
 {
-    int n;
-    cin>>n;
+    int n,k;
+    cin>>n>>k;
     vector<int>a(n);
     for(auto &i:a)
     cin>>i;
     multiset<int,greater<int>>s;
     vector<int>ans(n-k+1);
-    
+    for(int i=0;i<k;i++)
 }
